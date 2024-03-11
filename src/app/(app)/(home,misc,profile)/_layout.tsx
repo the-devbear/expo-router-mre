3cc724e384bc20ee3cd25615bafc303565b1b582
devbear <bjarne.haumann@gmail.com>
2024-03-11 20:14:22 +0100

    Test with segment, different stacks and inital route

--- src/app/(app)/(home,misc,profile)/_layout.tsx
+++ src/app/(app)/(home,misc,profile)/_layout.tsx
@@ -17,7 +17,14 @@ export default function DynamicLayout({
   segment,
 }: {
   segment: "(home)" | "(misc)" | "(profile)";
-}) {
+}): React.JSX.Element {
   console.log("DynamicLayout", segment);
-  return <Stack />;
+  switch (segment) {
+    case "(home)":
+      return <Stack initialRouteName="(home)/index" />;
+    case "(misc)":
+      return <Stack initialRouteName="misc/index" />;
+    case "(profile)":
+      return <Stack initialRouteName="profile/index" />;
+  }
 }
